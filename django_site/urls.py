c813988d7b97505ef2d14552a315486082eb1888
Laura Barluzzi <laura@koalacoder.com>
2017-04-25 18:25:02 +0000

    add blog app with a Post model

--- django_site/urls.py
+++ django_site/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('dashboard.urls'))
+    url(r'^', include('dashboard.urls')),
+    url(r'^blog/', include('blog.urls'))
 ]
